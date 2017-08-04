e44df628b65398167f0bf3b379de2063984d15e3
Max Datsun <unrusso@live.com>
2017-08-04 18:07:00 +0200

    Everything works, except ai.

--- index.js
+++ index.js
@@ -1,94 +1,217 @@
-var board = {
-        one: null,
-        two: null,
-        three: null,
-        four: null,
-        five: null,
-        six: null,
-        seven: null,
-        eight: null,
-        nine: null,
-    };
+//GLOBAL VARIABLES
+var charChoosen = false;
+var otherChoosen = false;
 
-class Player{
-    constructor(character, score, enemy){
-        this.character = character;
-        this.turn = true;
 
+//----------CLASSES------------
+class Board{
+    constructor(){
+        this.table = {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+            7: null,
+            8: null,
+            9: null,
+        };
     }
 
-    setOtherCharacter(){
-        this.otherCharacter = this.character === "0" ? "x" : "0";
+    clearTable(){
+        for(let i = 1; i < 10; i++){
+            this.table[i] = "";
+        }
+    }
+
+    isFull(){
+        for(var i in this.table){
+            if(this.table[i] === null){
+                return false;
+            } 
+        }
+        return true;
     }
 
 }
 
-class Pc{
-    constructor(character){
-        this.character = character;
-        this.depth = 9;
+class Player{
+    constructor(){
+        this.turn = true;
+
+    }
+    setOtherCharacter(){
+        this.otherCharacter = this.character === "0" ? "x" : "0";
     }
 
-};
+    reset(){
+        this.turn = true;
+    }
+
+}
+//------------------------------------------------------------
 
 
 
-
-var $square = $(".square");
-var charChoosen = false;
-var otherChoosen = false;
-
+var board = new Board();
 var player1 = new Player();
 
-var actions = [];
+
+
+newGame = () => {
+    player1.reset();
+    otherChoosen = false;
+    charChoosen = false;
+    checkForEvents();
+}
+
+
+hasWon = () => {
+    
+
+    function win(char){
+        $("#main-table").append("<tr><td class='action'>" + char + " won!</td></tr>");
+        $(".action").css("opacity", "1");
+
+        newGame();
+        
+
+    }
+
+    for(let i = 1; i < 10; i+= 3){
+        //ROWS
+        if(board.table[i] === 'x' && board.table[i+1] === 'x' && board.table[i+2] === 'x' ){
+            win('x');
+        }
+        else if(board.table[i] === '0' && board.table[i+1] === '0' && board.table[i+2] === '0'){
+            win('0');
+            
+        }
+    }
+    for(let i = 1; i < 10; i++){
+        //COLUMNS
+        if(board.table[i] === '0' && board.table[i+3] === '0' && board.table[i+6] === '0'){
+            win('0');
+            ;
+            
+        }
+        else if(board.table[i] === 'x' && board.table[i+3] === 'x' && board.table[i+6] === 'x'){
+            win('x');
+            
+        }
+
+    }
+    if(board.table[1] === 'x' && board.table[5] === 'x' && board.table[9] === 'x' ){
+            win('x');
+            
+            
+    }
+    else if(board.table[1] === '0' && board.table[5] === '0' && board.table[9] === '0' ){
+            win('0');
+            
+            
+    }
+    if(board.table[3] === 'x' && board.table[5] === 'x' && board.table[7] === 'x' ){
+            win('x');
+            
+            
+    }
+    if(board.table[3] === '0' && board.table[5] === '0' && board.table[7] === '0' ){
+            win('0');
+            
+            
+    }
+    
+}
+
+AiMove = () => {  
+    if(!board.isFull()){     
+        var move = Math.floor((Math.random() * 9)) +1;
+        console.log(board.table[move]);
+        console.log(board.table[move] === null);
+        if(board.table[move] === null){
+            console.log(board.table);
+            console.log(move);
+            board.table[move] = player1.otherCharacter;
+            $("#"+move).text(player1.otherCharacter);
+            player1.turn = true;
+            hasWon();
+        }
+        else{
+            AiMove();
+        }
+    }
+    
+}
+
 
 checkForEvents = () => {
     if(charChoosen === true && otherChoosen === true){
         player1.setOtherCharacter();
-        console.log(player1);
         
         if(player1.enemy === "friend"){
             playWithFriend();
         }
         
         else{
-            var pc = new Pc(player1.otherCharacter);
+
             playWithPc();
         }
     }
-}
-
-clear = (data) =>{
-    
-}
-
-createAction = (action) => {
-}
-
-playWithPc = () =>{
-    if(player1.turn === true){
-    $(this).text(player1.character);
-
-    player1.turn = false;
-    pc.makeMove();
+    else if (charChoosen === false && otherChoosen === false){
+        $("#bot-form").fadeIn();
+        $("#top-form").fadeIn();
     }
 }
 
+clear = () =>{
+    $("#main-table").html("");
+    $(".square").text("");
+}
+
+
+playWithPc = () =>{
+    for(let i = 1; i < 10; i++){
+        $("#" + i.toString()).text(board.table[i]);
+    }
+    $("td").click(function(){
+        if($(this).text() === ""){  
+            if(player1.turn === true){
+                $(this).text(player1.character);
+                board.table[this.id] = player1.character;
+                player1.turn = false;
+                hasWon();
+            }
+        }
+    if(player1.turn === false){
+        setTimeout(AiMove, 500);
+    }
+
+    });
+}
+
 playWithFriend = () =>{
-        $square.click(function(){
-            console.log($(this).text());
-            if($(this).text() === ""){     
+        for(let i = 1; i < 10; i++){
+            $("#" + i.toString()).text(board.table[i]);
+    }
+        $("td").click(function(){
+            if($(this).text() === ""){  
                 if(player1.turn === true){
                     $(this).text(player1.character);
+                    board.table[this.id] = player1.character;
                     player1.turn = false;
                 }
                 else{
-                    $(this).text(player1.otherCharacter);
+                    $(this).text(player1.otherCharacter); 
+                    board.table[this.id] = player1.otherCharacter;                 
                     player1.turn = true;
                 }
             }
+            hasWon();
+                
+        });
 
-    });
 }
 
 
@@ -98,6 +221,7 @@ $(document).ready(function(){
     $("button").click(function(){
         if($(this).hasClass("char")){
             player1.character = this.value;
+            player1.setOtherCharacter();
             charChoosen = true;
             $("#top-form").fadeOut(200);
 
@@ -115,6 +239,10 @@ $(document).ready(function(){
                 otherChoosen = true;
             }
         }
+
+    $("#clear").click(function(){
+        clear();
+    });
         checkForEvents();
     });
 
